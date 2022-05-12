d87b267d7a42034834580efceaf8a9d4845bc5d8
ffluk3 <lucas.shadler@flockfreight.com>
2022-05-12 16:10:48 -0700

    this commit should be ignored

--- check.js
+++ check.js
@@ -1,13 +1,13 @@
-const globSync = require('glob').sync;
-const path = require('path');
-const signale = require('signale');
-const fs = require('fs');
-const { incrementMap, printMap } = require('./utils');
+const globSync = require("glob").sync;
+const path = require("path");
+const signale = require("signale");
+const fs = require("fs");
+const { incrementMap, printMap } = require("./utils");
 
 function runEslint() {
   const [ruleId, globToRun] = process.argv.slice(2);
   if (!ruleId || !globToRun) {
-    signale.error('USAGE: ./check.js [rule-id] [glob]');
+    signale.error("USAGE: ./check.js [rule-id] [glob]");
     process.exit(1);
   }
 
@@ -18,10 +18,10 @@ function runEslint() {
   signale.start(`Scanning paths for ${ruleId}...`);
 
   files.forEach((file) => {
-    const relativePath = file.replace(`${process.cwd()}/`, '');
-    const moduleName = relativePath.split('/')[1];
+    const relativePath = file.replace(`${process.cwd()}/`, "");
+    const moduleName = relativePath.split("/")[1];
 
-    const textLines = fs.readFileSync(file).toString().split('\n');
+    const textLines = fs.readFileSync(file).toString().split("\n");
     textLines.forEach((line) => {
       if (line.startsWith(`// eslint-disable-next-line ${ruleId}`)) {
         resultMap = incrementMap(resultMap, moduleName);
--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const { CLIEngine } = require('eslint');
-const globSync = require('glob').sync;
-const path = require('path');
-const signale = require('signale');
-const fs = require('fs');
-const { printMap } = require('./utils');
+const { CLIEngine } = require("eslint");
+const globSync = require("glob").sync;
+const path = require("path");
+const signale = require("signale");
+const fs = require("fs");
+const { printMap } = require("./utils");
 
 const severity = {
-  0: 'None',
-  1: 'warn',
-  2: 'error',
+  0: "None",
+  1: "warn",
+  2: "error",
 };
 
 // TODO: allow this to be configurable by command line
 const options = {
   write: true,
-  rules: [
-    'import/no-cycle',
-  ],
+  rules: ["import/no-cycle"],
 };
 
 function writeEslintDisableCommentToLine(file, relevantMessages) {
-  const data = fs.readFileSync(file).toString().split('\n');
+  const data = fs.readFileSync(file).toString().split("\n");
 
   relevantMessages.forEach(({ line, ruleId }) => {
     data.splice(line - 1, 0, `// eslint-disable-next-line ${ruleId}`);
   });
 
-  const text = data.join('\n');
+  const text = data.join("\n");
 
   fs.writeFileSync(file, text);
 }
@@ -37,19 +35,23 @@ function runEslint() {
   // https://eslint.org/docs/6.0.0/developer-guide/nodejs-api
   const linter = new CLIEngine();
 
-  const report = linter.executeOnFiles(globSync(path.join(process.cwd(), args[0])));
+  const report = linter.executeOnFiles(
+    globSync(path.join(process.cwd(), args[0]))
+  );
 
-  const relevantResults = report.results
-    .filter((res) => res.messages.some((message) => message.severity === 2));
+  const relevantResults = report.results.filter((res) =>
+    res.messages.some((message) => message.severity === 2)
+  );
 
   const resultMap = {};
 
   relevantResults.forEach((result) => {
-    const parsedFilepath = result.filePath.replace(`${process.cwd()}/`, '');
+    const parsedFilepath = result.filePath.replace(`${process.cwd()}/`, "");
     signale.info(`${parsedFilepath}: `);
 
-    const relevantMessages = result.messages
-      .filter((message) => options.rules.includes(message.ruleId));
+    const relevantMessages = result.messages.filter((message) =>
+      options.rules.includes(message.ruleId)
+    );
 
     relevantMessages.forEach((ruleViolation) => {
       const sev = severity[ruleViolation.severity];
@@ -57,9 +59,11 @@ function runEslint() {
 
       signale[sev](`\t${line} ${ruleId} ${sev}`);
       // Generate mapping
-      const moduleName = parsedFilepath.split('/')[1];
+      const moduleName = parsedFilepath.split("/")[1];
 
-      resultMap[moduleName] = resultMap[moduleName] ? resultMap[moduleName] + 1 : 1;
+      resultMap[moduleName] = resultMap[moduleName]
+        ? resultMap[moduleName] + 1
+        : 1;
     });
 
     writeEslintDisableCommentToLine(result.filePath, relevantMessages);
